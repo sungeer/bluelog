de6c91f50e9212f44d7e171608ac2f29d6e1b26e
Want Sun <1013296326@qq.com>
2025-07-26 10:01:14 +0800

    add

--- viper/main.py
+++ viper/main.py
@@ -2,31 +2,40 @@ from gevent import monkey
 
 monkey.patch_all(thread=False, subprocess=False)
 
+import sys
 import signal
 
 from gevent.pywsgi import WSGIServer
 
+from viper.utils.util_log import logger
+
+
+def handle_uncaught_exception(exc_type, exc_value, exc_traceback):
+    logger.opt(exception=(exc_type, exc_value, exc_traceback)).critical('Critical exception, application will shutdown.')
+
+
+sys.excepthook = handle_uncaught_exception
+
 
 def application(environ, start_response):
-    # 获取请求方法
-    method = environ['REQUEST_METHOD']
-    # 获取请求路径
-    path = environ['PATH_INFO']
-    # 获取请求查询字符串
-    query = environ['QUERY_STRING']
-    # 获取请求头
-    headers = {k[5:]: v for k, v in environ.items() if k.startswith('HTTP_')}
-    print(headers)
-    # 获取请求体
     try:
-        length = int(environ.get('CONTENT_LENGTH', 0) or 0)
-    except (ValueError, TypeError):
-        length = 0
-    body = environ['wsgi.input'].read(length) if length > 0 else b''
+        method = environ['REQUEST_METHOD']
+        raise
+        path = environ['PATH_INFO']
+        query = environ['QUERY_STRING']
+        headers = {k[5:]: v for k, v in environ.items() if k.startswith('HTTP_')}
+        try:
+            length = int(environ.get('CONTENT_LENGTH', 0) or 0)
+        except (ValueError, TypeError):
+            length = 0
+        body = environ['wsgi.input'].read(length) if length > 0 else b''
 
-    # 这里可以处理业务逻辑
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b'Hello, world!\n']
+        start_response('200 OK', [('Content-Type', 'text/plain')])
+        return [b'Hello, world!\n']
+    except (Exception,):
+        logger.exception('error from verifying')
+        start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
+        return [b'Internal Server Error\n']
 
 
 def stop_server(signum, frame):
