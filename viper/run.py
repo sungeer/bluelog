602fd29d49c338b0bd3f6b02c903c32403b1e165
Want Sun <1013296326@qq.com>
2025-07-29 08:07:06 +0800

    add

--- viper/run.py
+++ viper/run.py
@@ -1,12 +1,4 @@
-from gevent import monkey
-
-monkey.patch_all(thread=False, subprocess=False)
-
-import signal
-from datetime import datetime
-
 from werkzeug.wrappers import Request
-from gevent.pywsgi import WSGIServer
 
 from viper.core.routes import response_for_path
 
@@ -15,17 +7,3 @@ def application(environ, start_response):
     request = Request(environ)
     response = response_for_path(request)
     return response(environ, start_response)
-
-
-def stop_server(signum, frame):
-    print('\nShutting down')
-    server.stop()
-    print('Finished server process')
-
-
-print(f'* Running on http://{'127.0.0.1'}:7788 (Press CTRL+C to quit)')
-print(f'* Using worker: gevent.pywsgi.WSGIServer')
-print(f'* Started at {datetime.now().isoformat(sep=' ', timespec='seconds')}')
-server = WSGIServer(('0.0.0.0', 7788), application)
-signal.signal(signal.SIGINT, stop_server)
-server.serve_forever()
--- viper/views/view_todo.py
+++ viper/views/view_todo.py
@@ -1,9 +1,14 @@
 from viper.wrappers.response import jsonify
 
+from viper.utils import util_pool
+from viper.services import service_todo
+
 
 def get_todos(request):
-    data = {'abc': 'qaz'}
-    return jsonify(data)
+    x = int(request.args.get('x', 1))
+    future = util_pool.pool_thread_common.submit(service_todo.func_a, x)
+    result = future.result(timeout=3)
+    return jsonify(result)
 
 
 route_dict = {
