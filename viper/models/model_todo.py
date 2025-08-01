79288907f286ab86a0c493c813c0929a4e11518d
wangxun <1013296326@qq.com>
2025-08-01 14:17:17 +0800

    add

--- viper/models/model_todo.py
+++ viper/models/model_todo.py
@@ -38,3 +38,61 @@ def get_todo_by_id():
         # data is None or {'id': 123, 'name': '张三'}
         data = result.mappings().first()
     return data
+
+
+def add_todo():
+    sql_str = '''
+        INSERT INTO your_table (name, age)
+        VALUES (:name, :age)
+    '''
+    params = {'name': '张三', 'age': 20}
+
+    with engine.begin() as conn:
+        result = conn.execute(text(sql_str), params)
+        # rowcount = result.rowcount  # 插入成功，受影响行数
+        inserted_id = result.lastrowid  # 自增主键id
+    return inserted_id
+
+
+def add_todos():
+    sql_str = '''
+        INSERT INTO your_table (name, age)
+        VALUES (:name, :age)
+    '''
+    data = [
+        {'name': '张三', 'age': 20},
+        {'name': '李四', 'age': 22},
+        {'name': '王五', 'age': 25},
+    ]
+
+    with engine.begin() as conn:
+        result = conn.execute(text(sql_str), data)
+        rowcount = result.rowcount  # 插入成功，受影响行数
+    return rowcount
+
+
+def del_todo():
+    sql_str = '''
+        DELETE FROM your_table
+        WHERE id=:id
+    '''
+    params = {'id': 5}
+
+    with engine.begin() as conn:
+        result = conn.execute(text(sql_str), params)
+        rowcount = result.rowcount  # 删除成功，受影响行数
+    return rowcount
+
+
+def update_todo():
+    sql_str = '''
+        UPDATE your_table
+        SET name=:name, age=:age
+        WHERE id=:id
+    '''
+    params = {'name': '李四', 'age': 22, 'id': 3}
+
+    with engine.begin() as conn:
+        result = conn.execute(text(sql_str), params)
+        rowcount = result.rowcount  # 更新成功，受影响行数
+    return rowcount
