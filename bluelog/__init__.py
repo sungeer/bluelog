81ae2da8d18eaf09375b8fa79236cf7c049a0c89
wangxun <1013296326@qq.com>
2025-10-03 16:37:00 +0800

    add

--- bluelog/__init__.py
+++ bluelog/__init__.py
@@ -1,20 +1,18 @@
 from flask import Flask
 
-from greybook.blueprints.admin import admin_bp
-from greybook.blueprints.auth import auth_bp
-from greybook.blueprints.blog import blog_bp
+from bluelog.blueprints.auth import auth_bp
+from bluelog.blueprints.blog import blog_bp
 from bluelog.core.errors import register_errors
 from bluelog.core.logging import register_logging
 from bluelog.settings import config
 
 
 def create_app(config_name):
-    app = Flask('shortly')
+    app = Flask('bluelog')
     app.config.from_object(config[config_name])
 
     # blueprints
     app.register_blueprint(blog_bp)
-    app.register_blueprint(admin_bp, url_prefix='/admin')
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
     # extensions
