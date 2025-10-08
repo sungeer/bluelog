97e21a0b0ae48ceb3ef6d5ad111e316a847ec130
Want Sun <1013296326@qq.com>
2025-10-08 15:28:23 +0800

    add

--- viper/cores/core_before.py
+++ viper/cores/core_before.py
@@ -12,3 +12,13 @@ def require_post(view_func):
         return view_func(request, *args, **kwargs)
 
     return _wrapped_view
+
+
+def require_get(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if request.method != 'GET':
+            return abort(405)
+        return view_func(request, *args, **kwargs)
+
+    return _wrapped_view
